c9ca4ec2b7f8902279d4c4694c50b72d8483bf3b
andrii <andrzja@gmail.com>
2023-10-24 20:39:20 +0300

    LESS8

--- script.js
+++ script.js
@@ -1,33 +1,32 @@
 "use strict"
 
-function delayedGreeting(duration, message) {
-    return new Promise((resolve, reject) => {
-        if (duration > 10000) {
-            reject(new Error("Затримка занадто довга"));
-        } else {
-            resolve(message);
-        }
-        setTimeout(() => {}, duration);
-    });
-}
-
 document.addEventListener('DOMContentLoaded', function() {
     const form = document.querySelector('form');
-    const delayInput = document.getElementById('delay');
-    const messageInput = document.getElementById('message');
+    const codeInput = document.getElementById('code');
+    const divShow = document.querySelector(".show");
 
     form.addEventListener('submit', function(event) {
         event.preventDefault();
 
-        const duration = parseInt(delayInput.value);
-        const message = messageInput.value;
+        const codeValue = codeInput.value;
 
-        delayedGreeting(duration, message)
-            .then((result) => {
-                console.log(message);
+        fetch(`https://http.dog/${codeValue}.json`)
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error('Помилка HTTP: ' + response.status);
+                }
+                return response.json(); // Припускаємо, що ви очікуєте JSON-відповідь.
             })
-            .catch((error) => {
-                console.error(error.message);
+            .then(data => {
+                const jpegUrl = data.image.jpg;
+                const img = document.createElement('img');
+                img.src = jpegUrl;
+                divShow.innerHTML = "";
+                divShow.appendChild(img);
+            })
+            .catch(error => {
+                console.error(error);
             });
     });
+
 });
